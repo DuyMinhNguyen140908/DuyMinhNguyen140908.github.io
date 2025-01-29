49bd3bbcc515c0dd932c9faa952e2d21be25a2e2
DuyMinhNguyen140908 <157292842+DuyMinhNguyen140908@users.noreply.github.com>
2025-01-30 01:48:41 +0700

    Update dp_connect.php

--- dp_connect.php
+++ dp_connect.php
@@ -1,11 +1,11 @@
 <?php
-$host = "localhost";
-$username = "root"; // Thay bằng tên người dùng của bạn
-$password = ""; // Thay bằng mật khẩu của bạn
-$dbname = "your_database_name"; // Tên cơ sở dữ liệu của bạn
+$servername = "localhost";
+$username = "root"; // Thay đổi với tên người dùng của bạn
+$password = ""; // Thay đổi với mật khẩu của bạn
+$dbname = "your_database_name"; // Thay đổi với tên cơ sở dữ liệu của bạn
 
 // Tạo kết nối
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Kiểm tra kết nối
 if ($conn->connect_error) {
