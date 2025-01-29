51e0ea573334eb5d566572d77fe2bd5c71becdab
DuyMinhNguyen140908 <157292842+DuyMinhNguyen140908@users.noreply.github.com>
2025-01-30 01:48:09 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-// Xóa session và chuyển hướng về trang login
+// Xóa session và chuyển hướng về trang đăng nhập
 session_unset();
 session_destroy();
 
-header("Location: login.php"); // Quay lại trang đăng nhập
+header("Location: login.html"); // Quay lại trang đăng nhập
 exit();
 ?>
